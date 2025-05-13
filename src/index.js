478f2800014e26e8d23f7bf0bcac069fcc3ccaf1
Crushed Lemon Dev <crushed.lemon.official@gmail.com>
2025-05-14 00:55:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_m7qBd8uy1",
   client_id: "127nttui414onihc7vrt9dbf8i",
-  redirect_uri: "https://www.crushed-lemon.com/apps/chess",
+  redirect_uri: "https://www.crushed-lemon.com/apps/chess/login-callback",
   response_type: "code",
   scope: "phone openid email",
 };
